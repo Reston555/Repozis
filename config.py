eba64e777d327414c1a41911414accb4094f6ac6
Reston555 <restrenwersewon@gmail.com>
2025-11-12 23:21:05 +0700

    Все данные бота

---
+++ config.py
@@ -0,0 +1,3 @@
+from telebot.apihelper import API_URL
+
+API_URL = "8543662058:AAFPuaBwnTdJxgBUbBgDepYzQLXhAS8-eRs"
---
+++ main.py
@@ -0,0 +1,13 @@
+import telebot
+from Config import API_URL
+bot= telebot.TeleBot(API_URL)
+@bot.message_handler(commands = ['start'])
+def start(message):
+    bot.send_message(message.chat.id,'555')
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id,'Доступные команды:\n" "/start — запустить бота и приветствие\n" "/help — показать это сообщение\n" "/about — информация о боте')
+@bot.message_handler(commands = ['about'])
+def about(message):
+    bot.send_message(message.chat.id,'Я простой телеграмм бот. Я ничего не умею. Но это пока что!')
+bot.polling()
